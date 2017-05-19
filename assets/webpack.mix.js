bac98b816278aaa4a34e7028f70c7427e286b4d1
Yohanes <yohaneszh@gmail.com>
2017-05-19 12:32:21 +0000

    exclude jquery from compilation

--- assets/webpack.mix.js
+++ assets/webpack.mix.js
@@ -1,5 +1,10 @@
 const { mix } = require('laravel-mix');
 
+mix.webpackConfig({
+  externals: {
+    'jquery': 'jquery'
+  }
+});
 
 mix.js('resources/assets/js/app.js', 'js')
   .sass('resources/assets/sass/main.scss', 'css')
